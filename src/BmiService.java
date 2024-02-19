a44bf6051e2e7658c6f73c385958ff6b5b516ee2
sulfurmagic <johnaul81@gmail.com>
2024-02-19 22:16:23 +0300

    reformated code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
 
     public int calculate(double heightM, int weightKg) {
-    double bmi = weightKg / (heightM * heightM);
+        double bmi = weightKg / (heightM * heightM);
 
-    return (int) bmi;
+        return (int) bmi;
     }
 }
